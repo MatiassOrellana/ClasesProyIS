300d2bba79f060178f3d9fcfb276bf86ad3f9667
DarkHormazabal <morgulgamer@gmail.com>
2024-03-19 12:56:42 -0300

    added filter

--- Induccion JavaScript/hola.js
+++ Induccion JavaScript/hola.js
@@ -17,7 +17,20 @@ arrayLetters.push("B");
 arrayLetters.push("C");
 arrayLetters.push("c");
 
+//funcion
+const miFuncion = () => {
+
+}
 //callback es una funcion que e pasa por otro argumento otra funcion
 arrayLetters.forEach((name, index) => {
     console.log(name, "Su posicion en el array es: ", index);
 });
+
+//nota: si hay mas de una linea de lógica, se debe usar llaves
+
+//filters
+const arrayFiltered = arrayLetters.filter((name) => name != "Matias");
+    console.log(arrayFiltered);
+
+
+
