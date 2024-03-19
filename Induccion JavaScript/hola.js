db6acc4be2fdbfcc49a1783859a6a60c1f7acda9
DarkHormazabal <morgulgamer@gmail.com>
2024-03-19 12:30:42 -0300

    intentando cambiar la variable

--- Induccion JavaScript/hola.js
+++ Induccion JavaScript/hola.js
@@ -3,4 +3,8 @@ console.log("hola mundo");
 //las variables
 //let, const, var
 
-const name = "Matias O";
+const name = "Matias";
+//java script no tiene etipado string es una desventaja enorme
+name = "Laurel";
+
+console.log(name);
