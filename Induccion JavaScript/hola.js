2f06f4b4da6415a294686a291ac7ede57083e486
DarkHormazabal <morgulgamer@gmail.com>
2024-03-19 12:35:34 -0300

    added commented in line 8

--- Induccion JavaScript/hola.js
+++ Induccion JavaScript/hola.js
@@ -5,7 +5,7 @@ console.log("hola mundo");
 
 const name = "Matias";
 //java script no tiene etipado string es una desventaja enorme
-
+//las comilas dobles para evitar los problemas de concatenacion
 let lastname = "orellana";
 
 console.log(name, lastname);
