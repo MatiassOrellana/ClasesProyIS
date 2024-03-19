7d06d01c13fc497373f992723c20dd614638d759
DarkHormazabal <morgulgamer@gmail.com>
2024-03-19 12:49:39 -0300

    added Array

--- Induccion JavaScript/hola.js
+++ Induccion JavaScript/hola.js
@@ -9,3 +9,15 @@ const name = "Matias";
 let lastname = "orellana";
 
 console.log(name, lastname);
+
+
+//arreglos
+let arrayLetters = ["Matias", "A"];
+arrayLetters.push("B");
+arrayLetters.push("C");
+arrayLetters.push("c");
+
+//callback es una funcion que e pasa por otro argumento otra funcion
+arrayLetters.forEach((name, index) => {
+    console.log(name, "Su posicion en el array es: ", index);
+});
